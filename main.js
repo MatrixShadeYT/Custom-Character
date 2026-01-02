9136c45d9f006ea80b7546758d16756258c7aff6
MatrixShadeYT <mpro9234@gmail.com>
2026-01-02 04:24:04 -0700

    v0.2

--- main.js
+++ main.js
@@ -1,61 +1,26 @@
-const dict = {
-    Main: {
-        Basics: {
-            Gender: ['select','null','feminine','masculine'],
-            Age: ['number',18,0,100]
-        },
-        Clothes: "Directory",
-        Body: "Directory"
-    },
-    Clothes: {Main: "Directory"},
-    Body: {Main: "Directory"}
-}
 var dir = "Main";
-function navigation(obj) {
-    var prop = document.getElementsByTagName('form')[0];
-    var navi = document.getElementsByTagName('div')[0];
-    if (obj.innerHTML in dict[dir]) {
-        if (dict[dir][obj.innerHTML] == "Directory") {
-            dir = obj.innerHTML;
-            navi.innerHTML = '';
-            navlist();
-        } else {
-            prop.innerHTML = proplist(obj.innerHTML);
-        }
-    } else {
-        console.error("It failed.")
-    }
-}
-function proplist(tag) {
+function proplist() {
     var stringy = '';
     for (let i = 0; i < Object.keys(dict[dir][tag]).length; i++) {
-        console.log('\ntag: '+Object.keys(dict[dir][tag])[i]);
-        console.log(dict[dir][tag][Object.keys(dict[dir][tag])[i]]);
         stringy += '<br><label for="'+Object.keys(dict[dir][tag])[i]+'">'+Object.keys(dict[dir][tag])[i]+': </label>';
         if (dict[dir][tag][Object.keys(dict[dir][tag])[i]][0] == 'select') {
-            stringy += '<select onchange="updVar(this)" id="'+Object.keys(dict[dir][tag])[i]+'" name="'+Object.keys(dict[dir][tag])[i]+'">';
-            for (let i = 0;
-                i < Object.keys(dict[dir][tag][Object.keys(dict[dir][tag])[i]]).length-1;
-                i++) {
+            stringy += '<select onchange="updVar(this)" id="'+
+            Object.keys(dict[dir][tag])[i]+'" name="'+
+            Object.keys(dict[dir][tag])[i]+'">';
+            stringy += '<option value="null" selected disabled>None</option>';
+            for (let x = 0; x < dict[dir][tag][Object.keys(dict[dir][tag])[i]].length-1; x++) {
                 stringy += '<option value="'+
-                    dict[dir][tag][Object.keys(dict[dir][tag])[i]][i]+
-                    '">'+
-                    dict[dir][tag][Object.keys(dict[dir][tag])[i]][i]+
-                    '</option>';
+                dict[dir][tag][Object.keys(dict[dir][tag])[i]][x+1]+'">'+
+                dict[dir][tag][Object.keys(dict[dir][tag])[i]][x+1]+'</option>';
             }
             stringy += '</select>';
         } else if (dict[dir][tag][Object.keys(dict[dir][tag])[i]][0] == 'number') {
             stringy += '<input onchange="updVar(this)" id="'+
-                Object.keys(dict[dir][tag])[i]+
-                '" game="'+
-                Object.keys(dict[dir][tag])[i]+
-                '" type="number" value="'+
-                dict[dir][tag][Object.keys(dict[dir][tag])[i]][1]+
-                '" min="'+
-                dict[dir][tag][Object.keys(dict[dir][tag])[i]][2]+
-                '" max="'+
-                dict[dir][tag][Object.keys(dict[dir][tag])[i]][3]+
-                '">';
+            Object.keys(dict[dir][tag])[i]+'" name="'+
+            Object.keys(dict[dir][tag])[i]+'" type="number" value="'+
+            vals[tag][i]+'" min="'+
+            dict[dir][tag][Object.keys(dict[dir][tag])[i]][1]+'" max="'+
+            dict[dir][tag][Object.keys(dict[dir][tag])[i]][2]+'">';
         }
     }
     return stringy;
@@ -67,5 +32,24 @@ function navlist() {
     }
 }
 function updVar(obj) {
-    console.log(obj);
+    console.log(obj.id+' = '+obj.value);
+    vals[tag][obj.id] = obj.value;
+}
+function navigation(obj) {
+    var prop = document.getElementsByTagName('form')[0];
+    var navi = document.getElementsByTagName('div')[0];
+    if (obj.innerHTML in dict[dir]) {
+        if (dict[dir][obj.innerHTML] == "Directory") {
+            dir = obj.innerHTML;
+            tag = obj.innerHTML;
+            navi.innerHTML = '';
+            prop.innerHTML = '';
+            navlist();
+        } else {
+            tag = obj.innerHTML;
+            prop.innerHTML = proplist();
+        }
+    } else {
+        console.error("It failed.")
+    }
 }
