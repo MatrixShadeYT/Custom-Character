dc0adeca88925e48c32a54a494e5e88d906a50f7
MatrixShadeYT <mpro9234@gmail.com>
2026-01-02 04:40:54 -0700

    v0.4

---
+++ values.js
@@ -0,0 +1,22 @@
+const dict = {
+    Main: {
+        Basics: {
+            Gender: ['select','feminine','masculine'],
+            Age: ['number',0,100]
+        },
+        Personality: "Directory",
+        Clothes: "Directory",
+        Body: "Directory"
+    },
+    Personality: {Main: "Directory"},
+    Clothes: {Main: "Directory"},
+    Body: {Main: "Directory"},
+}
+const vals = {
+    Basics: {
+        Gender: 'null',
+        Age: 0
+    },
+    Clothes: {},
+    Body: {}
+}
