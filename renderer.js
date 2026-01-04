bb77c2a1b9019cebdddc4d079f35f6fe72fc8c3d
MatrixShadeYT <mpro9234@gmail.com>
2026-01-04 00:14:47 -0700

    v0.16

--- renderer.js
+++ renderer.js
@@ -11,45 +11,8 @@ function animate() {
     ctx.lineTo(0.49*canvas.width,0.28*canvas.height);
     ctx.lineTo(0.37*canvas.width,0.28*canvas.height);
     ctx.lineTo(0.37*canvas.width,0.14*canvas.height);
-    ctx.fillStyle = vals['Basics']['Skin Color'];
+    ctx.fillStyle = '#cc1c1c';
     ctx.fill();
     ctx.stroke();
-    // Trape
-    ctx.beginPath();
-    ctx.moveTo((0.3*canvas.width),(0.33*canvas.height));
-    ctx.lineTo((0.57*canvas.width),(0.33*canvas.height));
-    ctx.lineTo((0.57*canvas.width)-(0.05*canvas.width),(0.52*canvas.height));
-    ctx.lineTo((0.3*canvas.width)+(0.05*canvas.width),(0.52*canvas.height));
-    ctx.lineTo((0.3*canvas.width),(0.33*canvas.height));
-    ctx.fillStyle = vals['Shirt']['Color'];
-    ctx.fill();
-    ctx.stroke();
-    // zoid
-    ctx.beginPath();
-    ctx.moveTo((0.31*canvas.width),(0.62*canvas.height));
-    ctx.lineTo((0.56*canvas.width),(0.62*canvas.height));
-    ctx.lineTo((0.56*canvas.width)-(0.04*canvas.width),(0.52*canvas.height));
-    ctx.lineTo((0.31*canvas.width)+(0.04*canvas.width),(0.52*canvas.height));
-    ctx.lineTo((0.31*canvas.width),(0.62*canvas.height));
-    ctx.fillStyle = vals['Pants']['Color'];
-    ctx.fill();
-    ctx.stroke();
-    // female
-    if (vals['Basics']['Gender'] == 'feminine') {
-        ctx.fillStyle = vals['Shirt']['Accent'];
-        ctx.beginPath();
-        ctx.arc(0.37*canvas.width,0.4*canvas.height,0.04*canvas.width,0,2*Math.PI);
-        ctx.fill();
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.arc(0.5*canvas.width,0.4*canvas.height,0.04*canvas.width,0,2*Math.PI);
-        ctx.fill();
-        ctx.stroke();
-    }
-    // pants
-    if (vals['Pants']['Bands'] == 'Belt') {
-        ctx.fillStyle = vals['Pants']['Accent'];
-        ctx.fillRect(0.34*canvas.width,0.51*canvas.height,0.19*canvas.width,0.03*canvas.height);
-    }
 }
 animate();
